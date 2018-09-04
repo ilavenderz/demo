1bff245f5a84b72052d23cd5c8e7be489f522b8f
istargazer <istargazeru@gmail.com>
2018-09-04 15:00:10 +0800

    完善try finally测试

--- src/com/stargazer/demos/java/TryFinally.java
+++ src/com/stargazer/demos/java/TryFinally.java
@@ -1,13 +1,20 @@
 package com.stargazer.demos.java;
 
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
 public class TryFinally {
-    public static void main(String[] args) {
+    private static final BlockingQueue<Integer> q1 = new LinkedBlockingQueue<>();
+    private static final BlockingQueue<Integer> q2 = new LinkedBlockingQueue<>();
+    public static void main(String[] args) throws InterruptedException {
         System.out.println(m_1());
         System.out.println(m_2());
         System.out.println(m_3());
         System.out.println(m_4());
-        m_5();
         m_6();
+        m_7();
+        m_5();
+        Thread.sleep(1000000);
     }
 
     public static int m_1(){
@@ -100,7 +107,12 @@ public class TryFinally {
                 System.out.println("m6-------------------------------");
                 try {
                     System.out.println("thread try block");
-                    Thread.sleep(100000);
+                    q1.put(1);
+                    while(true) {
+                        Integer i = q1.peek();
+                        if(null != i && 2 == i)
+                            break;
+                    }
                 } catch (Exception e) {
                     System.out.println("exception block");
                 } finally {
@@ -112,15 +124,51 @@ public class TryFinally {
         thread.start();
         while(true){
             try {
-                Thread.sleep(100);
-                if(thread.isAlive())
+                Thread.sleep(10);
+                if(1 == q1.take()){
                     thread.stop();
+                    q1.put(2);
+                    break;
+                }
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
     }
-    private static void stop(Thread thread) {
-        thread.stop();
+
+    public static void m_7() {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("m7-------------------------------");
+                try {
+                    System.out.println("interrupt thread try block");
+                    q2.put(1);
+                    while(true) {
+                        Integer i = q2.peek();
+                        if(null != i && 2 == i)
+                            break;
+                    }
+                } catch (Exception e) {
+                    System.out.println("interrupt exception block");
+                } finally {
+                    System.out.println("finally block");
+                }
+                System.out.println("m7-------------------------------");
+            }
+        });
+        thread.start();
+        while(true){
+            try {
+                Thread.sleep(10);
+                if(1 == q2.take()) {
+                    thread.interrupt();
+                    q2.put(2);
+                    break;
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
